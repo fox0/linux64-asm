cfb50941f807537f1a5d3169255fd4279c815adf
fox0 <red-fox0@yandex.ru>
2021-08-28 13:03:47 +0700

    +segment readable

--- hello.asm
+++ hello.asm
@@ -1,18 +1,34 @@
 format ELF64 executable 3
 entry _start
 
+include 'linux64.inc'
+
+segment executable
+align 8
+_start:
+    ; mov rax, sys_write
+    ; mov rdi, stdout
+    ; mov rsi, hello
+    ; mov rdx, hello.len
+    mov eax, 1
+    mov edi, eax
+    lea rsi, [hello]
+    mov edx, hello.len
+    syscall
+
+    ; mov rax, sys_exit
+    ; mov rdi, 0
+    mov eax, sys_exit
+    xor edi, edi
+    syscall
+
+segment readable
+; align 8
+virtual
+    align 8
+    __align = $ - $$
+end virtual
+db __align dup 0
+
 hello db "Hello fox", 10  ; \n
 .len = $ - hello
-
-_start:
-    ; size_t write(unsigned int fd, const char *buf, size_t count)
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, hello
-    mov rdx, hello.len
-    syscall
-
-    ; void exit(int exit_code)
-    mov rax, 60
-    mov rdi, 0
-    syscall
